4f245feeedde9ba382481da4ead8bd76a0d1e5b0
Tonn3y <tonykibet42@gmail.com>
2025-02-05 19:44:51 +0300

    constants.php

--- constants.php
+++ constants.php
@@ -6,6 +6,19 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php 
+    define("LOCATION","JKUAT is in JUJA <br>");
+    echo LOCATION;
+    // Const cannot be called inside a block like function or if statements
+    // define is gloal
+    define("CARS",["Ferrari-spyder","Porsche","Mclaren","Aston Martin <br>"]);
+    echo CARS[3];
+
+    function myTest() {
+        echo LOCATION ;
+    }
+
+    myTest();
+    ?>
 </body>
 </html>
