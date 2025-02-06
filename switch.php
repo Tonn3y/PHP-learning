1856c35e6dd0242169bb16ca1c9c62983863a25b
Tonn3y <tonykibet42@gmail.com>
2025-02-06 17:36:40 +0300

    switch.php

---
+++ switch.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $color = "Yellow";
+    switch($color) {
+        case "Blue":
+            echo "My favorite color is Blue";
+            break;
+        case "Red":
+            echo "My favorite color is Red";
+            break;
+        case "Yellow":
+            echo "Your favorite color is Yellow";
+            break;
+        case "Orange":
+            echo "Your favorite color is Orange";
+            break;        
+    }
+    ?>
+</body>
+</html>
