5f5d100681916f304170d7818e1459d9148f50c8
Tonn3y <tonykibet42@gmail.com>
2025-02-12 16:34:27 +0300

    loops.php

--- loops.php
+++ loops.php
@@ -20,4 +20,37 @@
         if ($a==9) continue;
         echo $a;
     }
+    for ($x=0;$x<=10;$x++) {
+        if ($x==4) break ;
+        echo "The number is $x <br>";
+    }
+    $fruits = array("Mango","Apple","Orange","WaterMelon");
+    foreach($fruits as $x) {
+        echo $x ,"<br>";
+    }
+
+    $profile = array("Sane"=>"21","Sterling"=>"23","Jesus"=>"19","Aguero"=>"26");
+
+    foreach($profile as $x => $y) {
+        echo "$x : $y <br>";
+    }
+
+    $colors = array ("Red","Yellow","Green","Orange","Purple","Grey");
+    foreach($colors as $a) {
+        if ($a == "Yellow") break;
+        echo "$a <br>";
+    }
+
+    foreach($colors as $b) {
+        if ($b == "Yellow") $b = "Pink";
+        // echo "$b <br>";
+    }
+    var_dump($colors);
+
+    // Introducing & will result in a change in the array
+    foreach($colors as & $b) {
+        if ($b == "Yellow") $b = "Pink";
+        // echo "$b <br>";
+    }
+    var_dump($colors);
 ?>
